778ff8dab4029a0404cafa74bbc340274d8ab629
nksfrank <niklas.frank@gmail.com>
2023-03-29 08:12:51 +0200

    feat: add Wrap utility component

---
+++ frontend/src/components/Wrap/index.tsx
@@ -0,0 +1,15 @@
+import { cloneElement, useMemo } from "react";
+
+type Props = {
+  when: boolean | (() => boolean);
+  wrap: JSX.Element;
+  children: JSX.Element;
+};
+export function Wrap({ when, wrap, children }: Props): JSX.Element {
+  return useMemo(() => {
+    if (typeof when === "function" ? when() : when) {
+      return cloneElement(wrap, wrap.props, children);
+    }
+    return children;
+  }, [children, when, wrap]);
+}
